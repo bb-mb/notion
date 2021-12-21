61aae85102eb7a628b6d106e1fc0e91a2ca2c8d9
soso01 <6125024@gmail.com>
2021-12-22 00:36:29 +0900

    <feat> mongodb 연결

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+console.log(process.env.DB_URL);
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
