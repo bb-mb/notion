810e1057cdc95b3bcbac820d63ac924414b3951b
soso01 <6125024@gmail.com>
2021-12-25 21:37:56 +0900

    <feat> block page search

--- server/src/lib/parse.ts
+++ server/src/lib/parse.ts
@@ -1,1 +1,19 @@
-export function getPageBlocks(page: object) {}
+export interface Ipage {
+  [keys: string]: any;
+  block: {
+    [id: string]: {
+      [keys: string]: any;
+      value: {
+        [keys: string]: any;
+        id: string;
+        type: string;
+      };
+    };
+  };
+}
+
+export function getPageBlocks(page: Ipage): string[] {
+  return Object.keys(page.block).filter((blockId) => {
+    return page.block[blockId].value.type === 'page';
+  });
+}
