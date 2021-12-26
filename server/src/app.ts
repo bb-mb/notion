f6f08587e19d174edaafda11a031115ceabd8bc6
soso01 <6125024@gmail.com>
2021-12-26 15:08:21 +0900

    <feat> mongoose 설치

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 
 import { pageRouter } from "./routes";
 
 const PORT = 3001;
 const app = express();
+mongoose.connect(process.env.DB_URL);
 
 app.use(express.json());
 app.use("/page", pageRouter);
