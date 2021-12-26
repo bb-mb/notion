f14cacf0d8964907bcc2c2f107cfbd49fa27bf88
soso01 <6125024@gmail.com>
2021-12-26 23:13:42 +0900

    <refactor> mongodb 연결

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,9 @@ import { pageRouter } from "./routes";
 
 const PORT = 3001;
 const app = express();
-mongoose.connect(process.env.DB_URL);
+mongoose
+  .connect(process.env.DB_URL as string)
+  .then(() => console.log("connected mongodb"));
 
 app.use(express.json());
 app.use("/page", pageRouter);
