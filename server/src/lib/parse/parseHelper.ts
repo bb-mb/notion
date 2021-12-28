7130ad484219dd257f834545311a493dbc4992fa
soso01 <6125024@gmail.com>
2021-12-29 00:48:10 +0900

    <feat> parse route 등록

--- server/src/lib/parse/parseHelper.ts
+++ server/src/lib/parse/parseHelper.ts
@@ -1,6 +1,5 @@
-import { defaultMapImageUrl } from "react-notion-x";
 import { getPageTitle } from "notion-utils";
-import { ImageBlock } from "notion-types";
+import { Block, ImageBlock } from "notion-types";
 
 import { INotionBlock, INotionPage } from "@/types/models";
 
@@ -38,4 +37,39 @@ export class ParseHelper {
       imageBlock.format?.display_source
     );
   }
+}
+
+// react-notion-x에서 가져옴
+export function defaultMapImageUrl(url: string, block: Block) {
+  if (!url) {
+    return "";
+  }
+
+  if (url.startsWith("data:")) {
+    return url;
+  }
+
+  if (url.startsWith("/images")) {
+    url = `https://www.notion.so${url}`;
+  }
+
+  // more recent versions of notion don't proxy unsplash images
+  if (!url.startsWith("https://images.unsplash.com")) {
+    url = `https://www.notion.so${
+      url.startsWith("/image") ? url : `/image/${encodeURIComponent(url)}`
+    }`;
+
+    const notionImageUrlV2 = new URL(url);
+    let table = block.parent_table === "space" ? "block" : block.parent_table;
+    if (table === "collection") {
+      table = "block";
+    }
+    notionImageUrlV2.searchParams.set("table", table);
+    notionImageUrlV2.searchParams.set("id", block.id);
+    notionImageUrlV2.searchParams.set("cache", "v2");
+
+    url = notionImageUrlV2.toString();
+  }
+
+  return url;
 }
--- server/src/routes/page.ts
+++ server/src/routes/page.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
+import { NotionAPI } from "notion-client";
+
+import { PageParser } from "@/lib/parse/parse";
+import { Page } from "@/models";
 
 export const router = Router();
 
-router.post("/parse");
+interface IParseRequestBody {
+  rootPageId: string;
+  userName: string;
+}
+
+router.post("/parse", async (req, res) => {
+  const { rootPageId, userName }: IParseRequestBody = req.body;
+  console.log(rootPageId);
+  const pageParser = new PageParser(Page, new NotionAPI());
+  const result = await pageParser.parse(rootPageId, userName);
+  res.send(result);
+});
 
 router.get("/", (req, res) => {
   res.send("pageRouter");
