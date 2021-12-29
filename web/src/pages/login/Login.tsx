dbe55a50e0d70654e1c8a0afb47b3fff78816cb9
soso01 <6125024@gmail.com>
2021-12-29 23:29:22 +0900

    <feat> 구글 로그인

--- web/src/pages/login/Login.tsx
+++ web/src/pages/login/Login.tsx
@@ -1,7 +1,29 @@
 import { NextPage } from "next";
 
 import { Layout } from "components/layout";
+import GoogleLogin, {
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+  GoogleLogout,
+} from "react-google-login";
 
 export const Login: NextPage = () => {
-  return <Layout>로그인</Layout>;
+  const loginResponse = (
+    res: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    console.log(res);
+  };
+
+  return (
+    <Layout>
+      로그인
+      <GoogleLogin
+        clientId="1012375576219-fv215tgm0mud85p4hqdjjrpdog9dkkjm.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={loginResponse}
+        onFailure={loginResponse}
+        cookiePolicy="single_host_origin"
+      />
+    </Layout>
+  );
 };
