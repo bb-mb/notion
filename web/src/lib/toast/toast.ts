d4d323bd9d2af36a863316b9ef5017a2a455878f
soso01 <6125024@gmail.com>
2022-01-01 20:44:51 +0900

    <feat> error 기본 메시지

--- web/src/lib/toast/toast.ts
+++ web/src/lib/toast/toast.ts
@@ -6,6 +6,6 @@ export function apiPromiseToast(callback: IApi) {
   return toast.promise(callback, {
     loading: "Loading...",
     success: (res) => res.data.msg,
-    error: (res) => res.data.msg,
+    error: (res) => res?.data?.msg || "잠시 후 다시 시도해주세요.",
   });
 }
