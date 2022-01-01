4107857f94a3c8dfe15f6c07d7b36ce65e473483
soso01 <6125024@gmail.com>
2022-01-01 15:42:30 +0900

    <refactor> firebase init 함수 분리

---
+++ web/src/lib/setting/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCVG_DQCoCeCapVeTaNnp5MX-5UnZrty7c",
+  authDomain: "notion-blog-336610.firebaseapp.com",
+  projectId: "notion-blog-336610",
+  storageBucket: "notion-blog-336610.appspot.com",
+  messagingSenderId: "1012375576219",
+  appId: "1:1012375576219:web:7cd18934a5594028cb703e",
+  measurementId: "G-91XZYQGWS3",
+};
+
+export function initFirebase() {
+  if (typeof window !== "undefined") {
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+  }
+}
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,31 +1,13 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { initFirebase } from "lib/setting";
+import { useEffect } from "react";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCVG_DQCoCeCapVeTaNnp5MX-5UnZrty7c",
-  authDomain: "notion-blog-336610.firebaseapp.com",
-  projectId: "notion-blog-336610",
-  storageBucket: "notion-blog-336610.appspot.com",
-  messagingSenderId: "1012375576219",
-  appId: "1:1012375576219:web:7cd18934a5594028cb703e",
-  measurementId: "G-91XZYQGWS3",
-};
-
-// Initialize Firebase
+import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  if (typeof window !== "undefined") {
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-  }
+  useEffect(() => {
+    initFirebase();
+  }, []);
 
   return <Component data-theme="light" {...pageProps} />;
 }
