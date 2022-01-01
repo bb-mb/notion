bd36f2139ef11cac59965f0944aa419b104d6e32
soso01 <6125024@gmail.com>
2022-01-01 15:55:58 +0900

    <feat> react-hot-toast 설치

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { Toaster } from "react-hot-toast";
 import type { AppProps } from "next/app";
 
 import { initFirebase } from "lib/setting";
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={new QueryClient()}>
       <Component data-theme="light" {...pageProps} />
+      <Toaster />
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
