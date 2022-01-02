af44151b39d397f2cbd589c7e03e97d940eaa1de
soso01 <6125024@gmail.com>
2022-01-02 13:25:42 +0900

    <refactor> user 조회 api 실 데이터 반환

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,11 @@
 import { checkAuthorization } from "@/middlewares";
+import { User } from "@/models";
 import { Router } from "express";
 
 export const router = Router();
 
-router.get("/", checkAuthorization, (req, res) => {
-  console.log(req.user);
+router.get("/", checkAuthorization, async (req, res) => {
+  const user = await User.findOne({ firebaseId: req.user?.firebaseId });
 
-  res.send("hi");
+  res.json({ msg: "조회 성공", value: user });
 });
